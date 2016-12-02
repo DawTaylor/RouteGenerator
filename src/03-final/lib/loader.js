2b96811403505cff6d4a9c13abedd41d6e8f0563
Jean Carlo Nascimento <jnascimento@gmail.com>
2016-12-02 00:41:07 -0200

    Só uma refatoradinha de leves :p
    ps: não testei ehheheheheheh

--- src/03-final/lib/loader.js
+++ src/03-final/lib/loader.js
@@ -1,16 +1,13 @@
-module.exports = (path, router) => {
-    var fs = require('fs')
+const fs = require('fs')
     
-    fs.readdir(path, (err, files) => {
+module.exports = (path, router) => 
+    fs.readdir(path, (err, files) => 
         files.map((file) => {
             var module = require(path + file)
             module.map((route, index) => {
-            router.set(route.path, route.method, (req, res, next) => {
-                module[index].action(req.params, (code, data) => {
-                    res.send(code, data)
-                })
-            })
-            })
+                router.set(route.path, route.method, (req, res, next) => 
+                    module[index].action(req.params, (code, data) => res.send(code, data))
+                )
+            )
         })
-    })
-}
+    )
